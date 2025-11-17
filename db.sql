efc308f0ae6c124a0ade04a68c6d02ed109e6940
Aditya Kotame <adityakotame17@gmail.com>
2025-11-17 11:11:35 +0530

    Created Database schema

--- db.sql
+++ db.sql
@@ -1,37 +1,30 @@
-DROP DATABASE IF EXISTS onlinefoodordering_db;
-CREATE DATABASE onlinefoodordering_db;
-USE onlinefoodordering_db;
+DROP DATABASE IF EXISTS blogmangement_db;
+CREATE DATABASE blogmangement_db;
+USE blogmangement_db;
 
 CREATE TABLE users(
-    uid INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(20),
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    full_name VARCHAR(50),
     email VARCHAR(50),
     password VARCHAR(100),
-    mobile CHAR(10)
+    phone_no VARCHAR(10),
+    created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE foodItems(
-    fid INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(20),
-    price DECIMAL(7,2),
-    description VARCHAR(300),
-    image VARCHAR(100)
+CREATE TABLE blogs(
+    blog_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(100),
+    contents VARCHAR(300),
+    created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    user_id INT,
+    category_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(category_id) ON DELETE CASCADE ON UPDATE CASCADE
+
 );
 
-CREATE TABLE orders(
-    oid INT PRIMARY KEY AUTO_INCREMENT,
-    uid INT,
-    odate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    deldate TIMESTAMP,
-    total_amount DECIMAL(9,2),
-    status ENUM('PENDING','PROCESSING','CANCELLED','DELIVERED') DEFAULT 'PENDING',
-    FOREIGN KEY (uid) REFERENCES users(uid) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-CREATE TABLE orderdetails(
-    oid INT,
-    fid INT,
-    quantity INT,
-    FOREIGN KEY (oid) REFERENCES orders(oid) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (fid) REFERENCES foodItems(fid) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE categories(
+    category_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(100),
+    description VARCHAR(300)
 );
